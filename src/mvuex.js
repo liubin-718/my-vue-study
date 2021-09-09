7222404df3779bf3c2fa61f056f2b0bd00d93518
liubin <liubin0718@126.com>
2021-09-09 11:11:44 +0800

    'my-store'

--- src/mvuex.js
+++ src/mvuex.js
@@ -1,54 +1,61 @@
-let Vue;
-
-class Store {
-  constructor(options) {
+/* 
+  插件
+  实现：state/mutations/actions/getter
+  创建store
+  数据响应式
+*/
+let Vue
+function install(_Vue) {
+  Vue = _Vue
+  //这样store执行时，就有了Vue，不用import
+  // 这也是为什么Vue.use 必须在新建Store之前
+  Vue.mixin({
+    beforeCreate(){
+      // 这样才能获取到传递进来的store
+      // 只有root元素才有store，所以判断一下
+      if(this.$options.store){
+        Vue.prototype.$store = this.$options.store
+      }
+    }
+  })
+}
+class Store{
+  constructor(options){
     this.state = new Vue({
-      data: options.state
-    });
-
-    this.mutations = options.mutations;
-    this.actions = options.actions;
-
+      data:options.state
+    })
+    this.mutations = options.mutations || {}
+    this.actions = options.actions
     options.getters && this.handleGetters(options.getters)
   }
 
-  // 声明为箭头函数，why？
+  // 这里使用箭头函数，后面actions实现时会有作用
   commit = (type, arg) => {
-    this.mutations[type](this.state, arg);
-  };
+    this.mutations[type](this.state, arg)
+  }
 
-  dispatch(type, arg) {
+  dispatch(type, arg){
     this.actions[type]({
       commit: this.commit,
       state: this.state
-    }, arg);
+    }, arg)
   }
 
-  handleGetters(getters) {
-    this.getters = {};
-    // 遍历getters所有key
-    Object.keys(getters).forEach(key => {
-        // 为this.getters定义若干属性，这些属性是只读的
-        // $store.getters.score
-        Object.defineProperty(this.getters, key, {
-            get: () => {
-                return getters[key](this.state);
-            }
-        })
+  handleGetters(getters){
+    this.getters = {}
+    // 遍历getters，为this.getters定义property
+    // 属性名就是选项中的key，只需定义get函数保证其可读性
+    Object.keys(getters).forEach( k => {
+      // 这些属性是只读的
+      Object.defineProperty(this.getters, k, {
+        // 注意依然是箭头函数
+        get: () => {
+          return getters[k](this.state)
+        }
+      })
     })
+
   }
 }
 
-function install(_Vue) {
-  Vue = _Vue;
-
-  Vue.mixin({
-    beforeCreate() {
-      if (this.$options.store) {
-        Vue.prototype.$store = this.$options.store;
-      }
-    }
-  });
-}
-
-export default { Store, install };
+export default{Store, install}
--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import Vue from "vue";
-import Vuex from "vuex";
+import Vuex from "./mvuex";
 
 Vue.use(Vuex);
 
